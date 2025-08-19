7c536ac810bad975ed7b9ea7e211f096e749206b
Fafa0905 <singhfathe93@gmail.com>
2025-08-19 16:55:08 +0200

    a

--- src/check_map.c
+++ src/check_map.c
@@ -6,7 +6,7 @@
 /*   By: fsingh <fsingh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 18:26:12 by sdakhlao          #+#    #+#             */
-/*   Updated: 2025/08/19 16:09:46 by fsingh           ###   ########.fr       */
+/*   Updated: 2025/08/19 16:48:04 by fsingh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ char	**dup_map(char **cub)
 
 int	ft_flood_fill(char **map, int y, int x)
 {
-	if (y < 0 || x < 0 || !map[y] || (map[y] && !map[y][x]) || map[y][x] == ' ')
+	if (y < 0 || x < 0 || !map[y] || (map[y] && !map[y][x]) || x >= ((int)ft_strlen(map[y]) - 1) || map[y][x] == ' ')
 		return (1);
 	else if (map[y][x] == '1')
 		return (0);
@@ -80,7 +80,7 @@ int	check_map(char **cub)
 				|| cub[i][j] == 'O' || cub[i][j] == 'E')
 			{
 				if (start_parse_map(cub, i, j) != 0)
-					return (printf("Map invalid\n"), free_tab(cub), 0);
+					return (printf("Map invalid\n"), 0);
 				else
 					return (1);
 			}
